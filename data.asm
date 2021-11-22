789293e57937db3025b558ec46d1518d0e490381
aguilindro <59779910+aguilindro@users.noreply.github.com>
2021-11-22 15:48:40 -0500

    Update data.asm

--- data.asm
+++ data.asm
@@ -7,11 +7,11 @@ prod_2: .asciiz "prod 2"
 prod_3: .asciiz "prod 3"
 prod_4: .asciiz "prod 4"
 
-productos: .word pulsera, sombrero de paja toquilla, billetera, pluma, camisa bordada, collar, aretes, cinturón 
-precios: .word 2, 30, 5, 1.50, 15, 12, 7.50, 10 
-cantidades: .word 10, 6, 10, 25, 8, 15, 10, 9
+productos: .word pulsera, billetera, pluma, collar, aretes, cinturón 
+precios: .word 3, 5, 2, 15, 7, 10 
+cantidades: .word 10, 10, 25, 15, 10, 9
 
  # MÃ­nima cantidad x producto antes de advertir
-min_stock: .word 3, 2, 2, 6, 2, 4, 2, 3
+min_stock: .word 3, 3, 7, 4, 3, 2
 
-total_productos: .word 8
+total_productos: .word 6
