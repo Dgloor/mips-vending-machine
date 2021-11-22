b5d93f25b8706359e7b6118d515e7a9b0edfd9ce
aguilindro <59779910+aguilindro@users.noreply.github.com>
2021-11-22 15:44:51 -0500

    Update data.asm
    productos, precios, cantidades, min_stock y total_productos agregados.

--- data.asm
+++ data.asm
@@ -7,11 +7,11 @@ prod_2: .asciiz "prod 2"
 prod_3: .asciiz "prod 3"
 prod_4: .asciiz "prod 4"
 
-productos: .word prod_1, prod_2, prod_3
-precios: .word 1, 3, 4
-cantidades: .word 10, 20, 5
+productos: .word pulsera, sombrero de paja toquilla, billetera, pluma, camisa bordada, collar, aretes, cinturón 
+precios: .word 2, 30, 5, 1.50, 15, 12, 7.50, 10 
+cantidades: .word 10, 6, 10, 25, 8, 15, 10, 9
 
  # MÃ­nima cantidad x producto antes de advertir
-min_stock: .word 3, 2, 2
+min_stock: .word 3, 2, 2, 6, 2, 4, 2, 3
 
-total_productos: .word 3
+total_productos: .word 8
